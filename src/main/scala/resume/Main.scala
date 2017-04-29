20e71330fa568e319dad6d55c3b6fb2345bb6bd1
pishen tsai <pishen02@gmail.com>
2017-04-30 00:43:53 +0800

    update skills

--- src/main/scala/resume/Main.scala
+++ src/main/scala/resume/Main.scala
@@ -179,7 +179,7 @@ object Main extends App {
           div(
             cls := "col-sm-10",
             marginTop := "20px",
-            Seq("Scala", "Java", "JavaScript", "Python", "SQL", "Apache Spark", "Play Framework", "Akka", "Amazon Web Service", "Machine Learning", "Emacs Lisp")
+            Seq("Scala", "Java", "JavaScript", "Python", "SQL", "HTML & CSS", "Apache Spark", "Play Framework", "Akka", "Amazon Web Service", "Machine Learning", "Emacs", "Ubuntu")
               .map(skill => span(cls := "label label-default", skill))
           )
         ),
